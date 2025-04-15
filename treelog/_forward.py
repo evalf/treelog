b8539eb98f494dca8762b4378904ea88366c7eb1
Gertjan van Zwieten <gertjanvanzwieten@evalf.com>
2025-04-15 10:25:08 +0200

    Remove copyright notices, vim tags

--- treelog/_forward.py
+++ treelog/_forward.py
@@ -1,23 +1,3 @@
-# Copyright (c) 2018 Evalf
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-
 import contextlib, typing, typing_extensions, tempfile, warnings, os
 from . import proto, _io
 
@@ -98,5 +78,3 @@ class FilterLog:
 
   def open(self, filename: str, mode: str, level: proto.Level) -> typing_extensions.ContextManager[typing.IO[typing.Any]]:
     return self._baselog.open(filename, mode, level) if self._passthrough(level) else _io.devnull(mode)
-
-# vim:sw=2:sts=2:et
