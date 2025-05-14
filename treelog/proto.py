645d407760b114402cb7f2f8e2032bf460e3acaa
Gertjan van Zwieten <gertjanvanzwieten@evalf.com>
2025-05-14 20:19:17 +0200

    Prepare transition to new Log protocol

--- treelog/proto.py
+++ treelog/proto.py
@@ -31,3 +31,31 @@ class Log(Protocol):
     def popcontext(self) -> None: ...
     def recontext(self, title: str) -> None: ...
     def write(self, msg: Union[str, Data], level: Level) -> None: ...
+
+
+# TRANSITIONAL, TEMPORARY
+class oldproto:
+    @classmethod
+    def fromnew(cls, NewLog):
+        return type(NewLog.__name__, (cls,), {'wrapped': NewLog})
+    def __init__(self, *args, **kwargs):
+        self.context = [self.wrapped(*args, **kwargs)]
+    @property
+    def current(self):
+        return self.context[-1]
+    def pushcontext(self, title):
+        self.context.append(self.current.branch(title))
+    def popcontext(self):
+        self.context.pop().close()
+    def recontext(self, title):
+        self.popcontext()
+        self.pushcontext(title)
+    def write(self, msg, level):
+        self.current.write(msg, level)
+    def __enter__(self):
+        return self
+    def __exit__(self, *args):
+        self.context.pop().close()
+        assert not self.context
+    def __getattr__(self, attr):
+        return getattr(self.context[0], attr)
